95fdb296ae8402156c03ea24909c93d517f6d1fb
Hyerim926 <ekfrhsk96@icloud.com>
2021-11-13 17:01:10 +0900

    mongoDB연결

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,6 +2,13 @@ const express = require('express') // express module 가져옴
 const app = express() //function을 이용해 새로운 app을 만듦
 const port = 5000 //port 번호
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://youtube:youtube@youtubeclone.nlzoz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
 // 루트 디렉토리에 오면 Hello World 출력
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요!')
