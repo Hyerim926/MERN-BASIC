4f7e5e2271c736734b1ab57131d8e28fc10b43fd
Hyerim926 <ekfrhsk96@icloud.com>
2021-11-13 16:22:02 +0900

    first commit

---
+++ boiler-plate/index.js
@@ -0,0 +1,13 @@
+const express = require('express') // express module 가져옴
+const app = express() //function을 이용해 새로운 app을 만듦
+const port = 5000 //port 번호
+
+// 루트 디렉토리에 오면 Hello World 출력
+app.get('/', (req, res) => {
+  res.send('Hello World! 안녕하세요!')
+})
+
+// localhost:5000에서 호출될 수 있게 해줌
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
