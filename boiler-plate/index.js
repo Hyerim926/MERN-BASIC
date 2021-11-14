bc9cd6b45198abc91b576f3dacc24e183f48540e
Hyerim926 <ekfrhsk96@icloud.com>
2021-11-14 15:59:51 +0900

    login 기능 구현

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,15 +2,19 @@ const express = require('express') // express module 가져옴
 const app = express() //function을 이용해 새로운 app을 만듦
 const port = 5000 //port 번호
 // const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require("./config/key");
+
 const { User } = require("./models/User");
 
-const config = require("./config/key");
+
 
 // app.use(bodyParser.urlencoded({extended: true}));
 // app.use(bodyParser.json());
 
 app.use(express.json()) //For JSON Requests, application/json
 app.use(express.urlencoded({extended: true})); //application/x-www-form-urlencoded
+app.use(cookieParser());
 
 
 const mongoose = require('mongoose')
@@ -21,23 +25,43 @@ mongoose.connect(config.mongoURI, {
 
 
 // 루트 디렉토리에 오면 Hello World 출력
-app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요!')
-})
+app.get('/', (req, res) => {res.send('Hello World! 안녕하세요!')})
 
 // 회원가입 창
-app.post('/register', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
-  //회원 가입할 떄 필요한 정보들을 client에서 가져오면 
-  //그것들을 데이터 베이스에 넣어줌
-  const user = new User(req.body)
+  // console.log('ping')
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
 
-  user.save((err, userInfo) => { //save() = mongodb 메서드
-    if(err) return res.json({success: false, err})
-    return res.status(200).json({ //200코드는 전송에 성공했다는 의미
-      success: true
+    // console.log('user', user)
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      // console.log('err',err)
+
+      // console.log('isMatch',isMatch)
+
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+      //비밀번호 까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다.  어디에 ?  쿠키 , 로컳스토리지 
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      })
     })
-  }) 
+  })
 })
 
 // localhost:5000에서 호출될 수 있게 해줌
